de7eabdae6ea308db22abbc508162a1273754a7c
0ddroom <96765335+0ddroom@users.noreply.github.com>
2026-05-14 17:29:02 +0900

    Add Supabase config

---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.BALANCE_GAME_CONFIG = {
+  SUPABASE_URL: "https://wcasoudiovcoaofgngob.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_6oIoXEx_Fe3aFbE8g62zyQ_tv3ADMSd",
+  PUBLIC_URL: "https://0ddroom.github.io/balance-game",
+};
